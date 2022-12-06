e6930ccfde092975a14d5fe4ea4847267341e68c
szymon3ek <sz.hunek.sh@gmail.com>
2022-12-06 07:28:04 +0100

    - adding css

--- server/api.js
+++ server/api.js
@@ -36,22 +36,35 @@ app.get('/add', (req, res) => {
     var cytatreq = req.query.cytat;
     var autorreq = req.query.autor;
 
-    res.json({ status: "OK", cytat: cytatreq, autor: autorreq });
+    if(cytatreq == undefined || autorreq == undefined) return res.json({status: "ERROR", error: "Nie podano wszystkich danych"});
+
 
     console.table({ cytatreq, autorreq });
 
     async function add() {
+
         const database = client.db(dbname);
         const collection = database.collection('cytaty');
         const result = await collection.insertOne({ cytat: cytatreq, autor: autorreq });
         console.log(result);
-    }
-
+            
+        if (result.acknowledged) {
+            res.json({ status: "OK", cytat: cytatreq, autor: autorreq });
+        } else {
+            res.json({ status: "ERROR", error: "Nie udalo sie dodac cytatu (dodawanie do bazy danych)" });
+        }
+    };
     add();
   
 });
 
+app.get('*', function (req, res) {
+    var err = new Error();
+    err.status = 404;
+    res.json({ status: "ERROR", error: "Nie znaleziono strony" });
+});
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.json({ status: "ERROR", message: "Nie podano parametrow" });
 });
