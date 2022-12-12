437ee85b271f9f65319377a4389b9ea9fef283b5
szymon3ek <sz.hunek.sh@gmail.com>
2022-12-12 20:55:02 +0100

    - css

--- public/script.js
+++ public/script.js
@@ -1,6 +1,5 @@
 
 function wyslij() {
-
   var response = grecaptcha.getResponse();
 
   var autor = document.getElementById("autor").value;
@@ -8,11 +7,25 @@ function wyslij() {
   var message = document.getElementById("message");
 
   if (autor == "" || cytat == "")
-    return (message.innerHTML = "Wypełnij wszystkie pola!");
+    return notsuccess("Nie wypelniles wszystkich danych!", "nie");
   if (response.length == 0)
-    return (message.innerHTML = 'Zaznacz pole "Nie jestem robotem"!');
+    return notsuccess('Brak potwierdzenia że nie jesteś botem!', 'nie');
 
-  
+
+  if (confirm(`Czy na pewno chcesz dodać cytat? AUTOR: ${autor} CYTAT: ${cytat}`) == true) {
+    wysylaniedobazy();
+  } else {
+    notsuccess('anulowano dodawanie cytatu!');
+  }
+}
+
+function wysylaniedobazy() {
+  var response = grecaptcha.getResponse();
+
+  var autor = document.getElementById("autor").value;
+  var cytat = document.getElementById("cytat").value;
+  var message = document.getElementById("message");
+
   fetch(
     `http://${window.location.hostname}:3000/add?autor=${autor}&cytat=${cytat}`
   ).then((response) =>
@@ -21,20 +34,32 @@ function wyslij() {
       if (data.status == "OK") {
         success(data);
       } else {
-        notsucces(data);
+        notsuccess("Wystąpił błąd!", "tak");
       }
     })
   );
 }
 
-function notsucces(data) {
+function notsuccess(powod, kasowanie) {
+
+  var audio = new Audio("etc\soundboard\error.mp3");
+
   var autor = document.getElementById("autor");
   var cytat = document.getElementById("cytat");
   var message = document.getElementById("message");
 
-  autor.value = "";
-  cytat.value = "";
-  message.innerHTML = `Wystąpił błąd!`;
+  if (kasowanie == "nie") {
+      message.style.color = "red";
+      message.innerHTML = powod;
+      audio.play();
+  } else {
+      autor.value = "";
+      cytat.value = "";
+      message.style.color = "red";
+      message.innerHTML = powod;
+      audio.play();
+  }
+
 }
 
 function success(data) {
@@ -44,5 +69,6 @@ function success(data) {
 
   autor.value = "";
   cytat.value = "";
+  message.style.color = "green";
   message.innerHTML = `Dodano cytat! <br><br> Cytat: ${data.cytat} <br> Autor: ${data.autor}`;
 }
